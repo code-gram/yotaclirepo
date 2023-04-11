c6b3ba7341aba71e396a3c6ddd92635469e2b6f2
Sharad Yuvraj Patil <sharad.patil@Yash.com>
2023-04-11 17:36:07 +0530

    footer changes

--- yotacli/src/components/dashboard/Dashboard.js
+++ yotacli/src/components/dashboard/Dashboard.js
@@ -13,8 +13,11 @@ function Dashboard() {
         <div className="col-xl-10 col-lg-10" style={{ padding: "0px 0px" }}>
           <Navbar />
           <Routing />
-          <div className="container-fluid" style={{padding:"0px"}}>
-          <Footer/>
+          <div
+            className="container-fluid"
+            style={{ paddingRight: "12px", paddingLeft: "0px" }}
+          >
+            <Footer />
           </div>
         </div>
       </div>
