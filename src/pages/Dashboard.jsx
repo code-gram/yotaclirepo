9736c279bae9622106ad90de44d0d88b6df43d98
Saikumar9122 <jason.sai33@gmail.com>
2023-08-22 17:55:01 +0530

    student dashboard

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Dashboard = () => {
+  return (
+    <div className="dashboard">   
+        <h1>DASHBOARD</h1>
+        <div className="card">
+          <div className="container">
+            <h4><b>Test Links</b></h4>            
+            <p>Test assigned by Trainee</p>
+            <p>click on view all </p>   
+            <button className="btn btn-success">View All</button>
+          </div>
+        </div>
+    </div>
+  );
+};
---
+++ src/pages/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div className="foot">
+      <div >
+        <p>
+          Copyright © 2022-2023 YOTA App Pvt.Ltd. All right reserved. Created by MURALI
+        </p>
+      </div>
+    </div>
+  );
+};
+
