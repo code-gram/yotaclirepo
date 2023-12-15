3bba7a9a1dc213a787ee79a0c238d67b1484006d
rickdev.majumder <rickdev.ma100@gmail.com>
2023-12-15 16:29:26 +0530

    Sidebar implementation modified with destructuring

--- yotacli/src/components/trainer/dashboard/common/TrainerSidebarSubMenu.js
+++ yotacli/src/components/trainer/dashboard/common/TrainerSidebarSubMenu.js
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./TrainerSidebar.css";
 
-const TrainerSidebarSubMenu = (props) => {
+const TrainerSidebarSubMenu = ({menuLink, children}) => {
     return (
         <li className="dropdown-item">
-            <Link to={props.menuLink}>{props.children}</Link>
+            <Link to={menuLink}>{children}</Link>
         </li>
     );
 }
