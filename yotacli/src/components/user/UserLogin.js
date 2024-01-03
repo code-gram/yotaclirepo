6b2606e576a16528b436768794d953ccb4db0974
rickdev.majumder <rickdev.ma100@gmail.com>
2024-01-03 15:29:17 +0530

    Login story implemented

--- yotacli/src/components/user/UserLogin.js
+++ yotacli/src/components/user/UserLogin.js
@@ -1,13 +1,11 @@
 import Form from 'react-bootstrap/Form';
 import { Link, useNavigate } from 'react-router-dom';
-import loginCss from './UserLogin.css'
 import LoginHeader from './UserHeader';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useState } from 'react';
-import { useDispatch } from "react-redux";
-import axios from 'axios';
-import { handleLoginToken } from "../../redux/features/userLogin/UserLoginSlice";
+import { useDispatch,useSelector } from "react-redux";
+import { userLoginData } from "../../redux/features/userLogin/UserLoginSlice";
 
 
 const UserLogin = () => {
@@ -15,28 +13,49 @@ const UserLogin = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [userCredentail, setUserCredentail] = useState('');
-    const loginUser = () => {
+    const userLogin = useSelector((state) => state.userLogin);
+    const dispatch = useDispatch();
+    const loginUser = (e) => {
+
+        e.preventDefault()
         let userCredentail = {
             username: username,
             password: password
         }
-        axios.post('/yota-api/users/authenticate', userCredentail)
-            .then((resp) => {
-                const loginData = resp.data;
-                sessionStorage.setItem('authToken', loginData.authToken);
-                sessionStorage.setItem('userRole', loginData.userRole);
-                if (resp.data) {
-                    toast("Login Success!");
-                    setTimeout(() => {
-                        navigate('/dashboard');
-                    }, 1000);
-                }
-            }).catch((error) => {
-                console.log(error);
-                toast("Invalid Credentail!!");
-                setUserCredentail()
-                navigate('/');
-            });
+        dispatch(userLoginData(userCredentail));
+        console.log("userLogin", userLogin);
+
+        if(userLogin.status === 'succeeded'){
+            // sessionStorage.setItem('token', userLogin.authToken);
+            // sessionStorage.setItem('userRole', userLogin.userRole);
+            toast("Login Success!");
+            setTimeout(() => {
+                navigate('/dashboard');
+            }, 1000);
+        }
+        if(userLogin.status === 'failed'){
+            toast("Invalid Credentail!!");
+            setUserCredentail()
+            navigate('/');
+        }
+        // axios.post('/yota-api/users/authenticate', userCredentail)
+        //     .then((resp) => {
+        //     const loginData = resp.data;
+        //     dispatch(handleLoginToken(loginData));
+        //     sessionStorage.setItem('token', loginData.authToken);
+        //     sessionStorage.setItem('userRole', loginData.userRole);
+        //     if (resp.data) {
+        //         toast("Login Success!");
+        //         setTimeout(() => {
+        //             navigate('/dashboard');
+        //         }, 1000);
+        //     }
+        // }).catch((error) => {
+        //     console.log(error);
+        //     toast("Invalid Credentail!!");
+        //     setUserCredentail()
+        //     navigate('/');
+        // });
 
     }
 
@@ -63,14 +82,15 @@ const UserLogin = () => {
                                             <Form.Label>Password:</Form.Label>
                                             <Form.Control type="password" onChange={(e) => { setPassword(e.target.value) }} placeholder="Password" />
                                         </Form.Group>
-                                    </Form>
-                                    <div className='text-center'>
-                                        <button type='submit' className='btn btn-success login-btn' onClick={loginUser}>Login</button>
+                                        <div className='text-center'>
+                                        <button type='button' className='btn btn-success login-btn' onClick={loginUser}>Login</button>
                                         <span className='d-flex justify-content-center'>
                                             <Link to='/forgotPassword'>Forgot Password</Link>&nbsp;&nbsp;|
                                             New User?&nbsp;<Link to='/register'>Register</Link>
                                         </span>
                                     </div>
+                                    </Form>
+                                  
                                 </div>
                             </div>
                         </div>
--- yotacli/src/redux/features/userLogin/UserLoginSlice.js
+++ yotacli/src/redux/features/userLogin/UserLoginSlice.js
@@ -1,20 +1,45 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+
+export const userLoginData = createAsyncThunk(
+  "createLoginData",
+  async (userCredentail) => {
+    const response = await axios.post('/yota-api/users/authenticate', userCredentail)     
+     const data = await response.data;
+     console.log(data);
+     return data;
+  }
+);
 
 export const userLogin = createSlice({
   name: "userLogin",
   initialState: {
     authToken: "",
-    userRole: ""
+    userRole: "",
+    status: false
   },
 
-  reducers: {
-    handleLoginToken: (state, action) => {
-      state.authToken = action.payload.authToken;
-      state.userRole = action.payload.userRole;
-    },
-  },
+  extraReducers(builder) {
+    builder
+      .addCase(userLoginData.pending, (state, action) => {
+        state.status = 'loading'
+        
+      })
+      .addCase(userLoginData.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        // Add any fetched posts to the array
+        state.authToken = action.payload.authToken;
+        state.userRole = action.payload.userRole;
+        console.log("authToken  ", action.payload.authToken)
+        console.log("userRole  ", action.payload.userRole)
+
+      })
+      .addCase(userLoginData.rejected, (state, action) => {
+        state.status = 'failed'
+        state.error = action.error.message
+      })
+  }
 
 });
-
-export const { handleLoginToken } = userLogin.actions;
-export default userLogin.reducer
+export default userLogin.reducer;
